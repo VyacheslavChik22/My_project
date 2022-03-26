ec8a215e125ffd7efd7e76fb293a8081e25dabf9
VyacheslavCHik <asyofka@mail.ru>
2022-03-26 17:09:28 +0300

    create my projekt

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        for(int i = 1; i <= 10; i++){ // Посчитаем от 1 до 10;
+            System.out.println(i);
+        }
 
     }
 }
