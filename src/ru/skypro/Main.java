9256e048de1f19a342ac169c75ec6a68d95a32c9
VyacheslavCHik <asyofka@mail.ru>
2022-03-26 17:12:50 +0300

    Count to ten

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        for(int i = 1; i <= 10; i++){ // Посчитаем от 1 до 10;
-            System.out.println(i);
+        for(int i = 1; i <= 10; i++){ // Посчитаем от одного  до десяти;
+            System.out.print(i + " "); // Выводим в строчку
         }
 
     }
